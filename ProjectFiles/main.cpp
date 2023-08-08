064e7a1520676086b31b15de033bc4d658f9e4b3
Papa-DMan <danjmahaney@gmail.com>
2023-08-07 20:05:49 -0400

    docs and formatting

--- ProjectFiles/main.cpp
+++ ProjectFiles/main.cpp
@@ -1,42 +1,42 @@
 #include <FreeRTOS.h>
+#include <mbedtls/pem.h>
+#include <mbedtls/pk.h>
+#include <mbedtls/rsa.h>
+#include <pico/rand.h>
 #include <queue.h>
 #include <stdio.h>
 #include <task.h>
-
-#include "lwip/ip4_addr.h"
-#include "pico/cyw43_arch.h"
-#include "pico/stdlib.h"
-#include "piodmx.h"
-#include "hardware/timer.h"
-#include "pico/util/datetime.h"
 #include <time.h>
-#include "lwip/tcpip.h"
-#include "lwip/api.h"
-#include "lwip/opt.h"
+
+#include <array>
+#include <cstring>
+#include <map>
+#include <set>
 #include <utility>
 #include <vector>
-#include <array>
-#include <map>
+
+#include "EEPROM.h"
+#include "core_json.h"
+#include "dhcpserver.h"
+#include "dnsserver.h"
+#include "encryption_keys.h"
+#include "hardware/timer.h"
+#include "http_state.h"
+#include "lwip/api.h"
 #include "lwip/apps/fs.h"
 #include "lwip/apps/httpd.h"
 #include "lwip/apps/mdns.h"
-#include "dhcpserver.h"
-#include "dnsserver.h"
-#include "http_state.h"
-#include "core_json.h"
-#include <cstring>
-#include <set>
+#include "lwip/ip4_addr.h"
+#include "lwip/opt.h"
+#include "lwip/tcpip.h"
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+#include "pico/util/datetime.h"
+#include "piodmx.h"
 
-#include <pico/rand.h>
-#include <mbedtls/rsa.h>
-#include <mbedtls/pk.h>
-#include <mbedtls/pem.h>
-#include "encryption_keys.h"
-#include "EEPROM.h"
-
-//default config values
+// default config values
 static EEPROMClass eeprom;
-char calcCheckSum(char *data, size_t len) {
+char calcCheckSum(char* data, size_t len) {
     char checksum = 0;
     for (int i = 0; i < len; i++) {
         checksum += data[i];
@@ -77,21 +77,19 @@ enum HTTPDREQ {
     APICONF
 };
 
-
 static mbedtls_pem_context pem;
 static mbedtls_pk_context pk;
-
 
 static HTTPDREQ httpdreq = API;
 
 static DMX dmx;
 
-void dmx_task(void *pvParameters) {
-    //xTaskCreate(dmx_loop, "dmx_loop", 2048, NULL, 3, NULL);
+void dmx_task(void* pvParameters) {
+    // xTaskCreate(dmx_loop, "dmx_loop", 2048, NULL, 3, NULL);
     uint8_t zero[512];
     memset(zero, 0, 512);
     xQueueSend(dmxQueue, zero, 0);
-    while(1) {
+    while (1) {
         uint8_t data[512];
         xQueueReceive(dmxQueue, data, portMAX_DELAY);
         while (dmx.busy()) {
@@ -106,8 +104,8 @@ void dmx_task(void *pvParameters) {
  * @brief Callback function for the httpd server receiving a POST request
  * @post The httpdreq vairable is set to the appropriate value
  * @return ERR_OK if the request is valid, ERR_VAL otherwise
-*/
-err_t httpd_post_begin(void * connection, const char *uri, const char * http_request, u16_t http_request_len, int content_len, char * response_uri, u16_t response_uri_len, u8_t *post_auto_wnd) {
+ */
+err_t httpd_post_begin(void* connection, const char* uri, const char* http_request, u16_t http_request_len, int content_len, char* response_uri, u16_t response_uri_len, u8_t* post_auto_wnd) {
     if (strncmp(uri, "/api", 4) == 0) {
         if (strncmp(uri, "/api/auth", 9) == 0) {
             httpdreq = HTTPDREQ::APIAUTH;
@@ -128,9 +126,8 @@ err_t httpd_post_begin(void * connection, const char *uri, const char * http_req
  * @param keys The key string buffer to be parsed
  * @param keysLength The length of the key string
  * @post The dmxQueue is populated with the DMX frame to be sent
-*/
+ */
 void processKeys(char* keys, size_t keysLength) {
-
     char* token;
     std::vector<char*> tokens;
     std::vector<uint16_t> channels;
@@ -138,7 +135,7 @@ void processKeys(char* keys, size_t keysLength) {
     bool isTHRU = false;
     uint8_t dmxFrame[512];
     memset(dmxFrame, 0, 512);
-    
+
     token = strtok(keys, " ");
     while (token != nullptr) {
         tokens.push_back(token);
@@ -147,15 +144,12 @@ void processKeys(char* keys, size_t keysLength) {
     for (const auto& t : tokens) {
         if (strncmp(t, "release", 7) == 0) {
             break;
-        }
-        else if (strncmp(t, "AND", 3) == 0) {
+        } else if (strncmp(t, "AND", 3) == 0) {
             continue;
-        }
-        else if (strncmp(t, "AT", 2) == 0) {
+        } else if (strncmp(t, "AT", 2) == 0) {
             isLEVEL = true;
             continue;
-        }
-        else if (strncmp(t, "FULL" , 4) == 0) {
+        } else if (strncmp(t, "FULL", 4) == 0) {
             for (auto d : channels) {
                 dmxFrame[d] = 255;
             }
@@ -163,11 +157,9 @@ void processKeys(char* keys, size_t keysLength) {
             channels.clear();
             isLEVEL = false;
             continue;
-        }
-        else if (strncmp(t, "THRU", 4) == 0) {
+        } else if (strncmp(t, "THRU", 4) == 0) {
             isTHRU = true;
-        }
-        else {
+        } else {
             if (isLEVEL) {
                 uint16_t level = atoi(t);
                 for (auto d : channels) {
@@ -176,24 +168,21 @@ void processKeys(char* keys, size_t keysLength) {
                 captured.insert(channels.begin(), channels.end());
                 channels.clear();
                 isLEVEL = false;
-            } else
-            if (isTHRU) {
+            } else if (isTHRU) {
                 uint16_t channel = atoi(t);
                 if (channel >= 1 && channel <= 512)
-                for (int i = channels.back() + 1; i <= channel; i++) {
-                    channels.push_back(i);
-                }
+                    for (int i = channels.back() + 1; i <= channel; i++) {
+                        channels.push_back(i);
+                    }
 
                 isTHRU = false;
-            }
-            else {
+            } else {
                 uint16_t channel = atoi(t);
                 channels.push_back(channel);
             }
         }
     }
     xQueueSend(dmxQueue, dmxFrame, portMAX_DELAY);
-
 }
 
 /**
@@ -201,9 +190,9 @@ void processKeys(char* keys, size_t keysLength) {
  * @param json The json buffer to be parsed
  * @param json_len The length of the json buffer
  * @post The dmxQueue is populated with the DMX frame to be sent
-*/
+ */
 void parseAPIKeysRequest(char* json, int json_len) {
-    char *value;
+    char* value;
     size_t value_len;
     JSONStatus_t result;
     result = JSON_Validate(json, json_len);
@@ -221,26 +210,26 @@ void parseAPIKeysRequest(char* json, int json_len) {
  * @param password_len The length of the password buffer
  * @post the result buffer is populated with the decrypted password and needs to be freed by the caller
  * @return The decrypted password if the password is valid, nullptr otherwise
- * 
-*/
-char * decryptPassword(char* password, size_t password_len) {
+ *
+ */
+char* decryptPassword(char* password, size_t password_len) {
     if (rfu_config.encrypt == false) {
         return password;
     } else {
         uint8_t* result = new uint8_t[512];
         size_t olen;
         size_t decryptedPassword_len;
-        mbedtls_pk_init( &pk );
+        mbedtls_pk_init(&pk);
         mbedtls_pem_init(&pem);
         int readPem = mbedtls_pk_parse_key(&pk, private_key, sizeof(private_key), NULL, 0);
         if (readPem != 0) {
             return nullptr;
         }
-        int status = mbedtls_pk_decrypt(&pk, (const unsigned char*)password, password_len, result, &olen, sizeof(result),nullptr, nullptr);
+        int status = mbedtls_pk_decrypt(&pk, (const unsigned char*)password, password_len, result, &olen, sizeof(result), nullptr, nullptr);
         if (status != 0) {
             return nullptr;
         }
-        char *s = strstr((char*)result, "salt="); //our request is salted so we need to reject non-salted requests
+        char* s = strstr((char*)result, "salt=");  // our request is salted so we need to reject non-salted requests
         if (s == nullptr) {
             return nullptr;
         }
@@ -250,13 +239,13 @@ char * decryptPassword(char* password, size_t password_len) {
 }
 
 /**
- * 
+ *
  * @brief Parses/verifies the api/auth request and returns the appropriate response code
  * @param json The json buffer to be parsed
  * @param json_len The length of the json buffer
  * @return 200 if the password is correct, 400 otherwise
- * 
-*/
+ *
+ */
 uint16_t parseAPIAuthRequest(char* json, int json_len) {
     char* value;
     size_t value_len;
@@ -276,8 +265,13 @@ uint16_t parseAPIAuthRequest(char* json, int json_len) {
     return 400;
 }
 
-void write_config_task(void *pvParameters) {
-    //enter critical section
+/**
+ * @brief Writes the config to the EEPROM must be spawned with configMAX_PRIORITIES - 1 priority
+ * @param pvParameters Unused
+ * @post The config is written to the EEPROM
+ */
+void write_config_task(void* pvParameters) {
+    // enter critical section
     taskENTER_CRITICAL();
     eeprom.put(0, rfu_config);
     eeprom.commit();
@@ -286,7 +280,7 @@ void write_config_task(void *pvParameters) {
 }
 
 uint16_t parseAPIConfRequest(char* json, int json_len) {
-    char * value;
+    char* value;
     size_t value_len;
     JSONStatus_t result;
     rfu_config_t config;
@@ -338,15 +332,14 @@ uint16_t parseAPIConfRequest(char* json, int json_len) {
  * @param p The packed buffer containing the data (NEEDS TO BE FREED)
  * @post The response header is written to the connection and the packed buffer is freed
  * @return ERR_OK
-*/
-err_t httpd_post_receive_data(void* connection, struct pbuf *p) {
+ */
+err_t httpd_post_receive_data(void* connection, struct pbuf* p) {
     char* data = (char*)p->payload;
     switch (httpdreq) {
         case HTTPDREQ::APIAUTH: {
             if (parseAPIAuthRequest(data, p->len) == 200) {
                 snprintf(((http_state*)connection)->hdrs[0], ((http_state*)connection)->hdr_content_len[0], "HTTP/1.1 200 OK");
-            }
-            else {
+            } else {
                 snprintf(((http_state*)connection)->hdrs[0], ((http_state*)connection)->hdr_content_len[0], "HTTP/1.1 400 Bad Request");
             }
             break;
@@ -379,62 +372,60 @@ err_t httpd_post_receive_data(void* connection, struct pbuf *p) {
  * @param connection The connection pointer to the client
  * @param response_uri The buffer to write the response uri to
  * @param response_uri_len The length of the response uri buffer
- * 
-*/
+ *
+ */
 void httpd_post_finished(void* connection, char* response_uri, u16_t response_uri_len) {
-    http_state *hs = (http_state*)connection;
+    http_state* hs = (http_state*)connection;
     snprintf(hs->hdrs[1], hs->hdr_content_len[1], "Content-type: text/html");
     snprintf(hs->hdrs[2], hs->hdr_content_len[2], "");
     snprintf(response_uri, response_uri_len, "/index.html");
 }
 
-void wifi_init_task(void *) {
-    if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {                                      //init wifi module with country code
-        //printf("CYW43 initalization failed\n");
+void wifi_init_task(void*) {
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {  // init wifi module with country code
+        // printf("CYW43 initalization failed\n");
     }
-    cyw43_wifi_pm(&cyw43_state, 0xA11140);                                                      //disable powersave mode
+    cyw43_wifi_pm(&cyw43_state, 0xA11140);  // disable powersave mode
     if (rfu_config.ap_mode) {
-        cyw43_arch_enable_ap_mode(rfu_config.ssid, rfu_config.password, CYW43_AUTH_WPA2_AES_PSK);                         //enable AP mode
+        cyw43_arch_enable_ap_mode(rfu_config.ssid, rfu_config.password, CYW43_AUTH_WPA2_AES_PSK);  // enable AP mode
 
-        IP_ADDR4(ip_2_ip4(&gw), 192, 168, 4, 1);                                                    //set IP address                           
-        IP_ADDR4(ip_2_ip4(&mask), 255, 255, 255, 0);                                                //set netmask       
-        netif_set_addr(netif_default, &gw, &mask, &gw);                                             //set netif ip netmask and gateway    
-        dhcp_server_init(&dhcp, &gw, &mask);                                                        //start DHCP server
-        dns_server_init(&dns, &gw);                                                                 //start DNS server              
-        netif_set_hostname(netif_default, "rfunit");                                                //set hostname
+        IP_ADDR4(ip_2_ip4(&gw), 192, 168, 4, 1);         // set IP address
+        IP_ADDR4(ip_2_ip4(&mask), 255, 255, 255, 0);     // set netmask
+        netif_set_addr(netif_default, &gw, &mask, &gw);  // set netif ip netmask and gateway
+        dhcp_server_init(&dhcp, &gw, &mask);             // start DHCP server
+        dns_server_init(&dns, &gw);                      // start DNS server
+        netif_set_hostname(netif_default, "rfunit");     // set hostname
     } else {
         vTaskDelay(1000);
-        cyw43_arch_enable_sta_mode();                                                               //enable STA mode
-        cyw43_arch_wifi_connect_async(rfu_config.ssid, rfu_config.password, CYW43_AUTH_WPA2_AES_PSK);                     //connect to AP
+        cyw43_arch_enable_sta_mode();                                                                  // enable STA mode
+        cyw43_arch_wifi_connect_async(rfu_config.ssid, rfu_config.password, CYW43_AUTH_WPA2_AES_PSK);  // connect to AP
         printf("Connecting to %s\n", rfu_config.ssid);
-        while(cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA) != CYW43_LINK_UP) {   
-            printf("Status: %d\n", cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA));           //wait for connection
+        while (cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA) != CYW43_LINK_UP) {
+            printf("Status: %d\n", cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA));  // wait for connection
             vTaskDelay(1000);
         }
         printf("\n");
-        printf("IP Address: %s\n", ip4addr_ntoa(&netif_default->ip_addr));                          //print IP address
-        netif_set_hostname(netif_default, "rfunit");                                                //set hostname
-        dhcp_start(netif_default);                                                                  //start DHCP client
+        printf("IP Address: %s\n", ip4addr_ntoa(&netif_default->ip_addr));  // print IP address
+        netif_set_hostname(netif_default, "rfunit");                        // set hostname
+        dhcp_start(netif_default);                                          // start DHCP client
         printf("dhcp started\n");
-        while(!dhcp_supplied_address(netif_default)) {                                              //wait for DHCP to finish
+        while (!dhcp_supplied_address(netif_default)) {  // wait for DHCP to finish
             vTaskDelay(1000);
             printf(".");
         }
     }
-    printf("IP Address: %s\n", ip4addr_ntoa(&netif_default->ip_addr));                          //print IP address
-    //mdns_resp_init();
-    //mdns_resp_add_netif(netif_default, "rfunit");
+    printf("IP Address: %s\n", ip4addr_ntoa(&netif_default->ip_addr));  // print IP address
+    // mdns_resp_init();
+    // mdns_resp_add_netif(netif_default, "rfunit");
 
-    dmxQueue = xQueueCreate(5, 512);                                                            //create queue for DMX frames
-    dmx.begin(2/*, 6*/);                                                                        //init DMX                
+    dmxQueue = xQueueCreate(5, 512);  // create queue for DMX frames
+    dmx.begin(2 /*, 6*/);             // init DMX
 
-    xTaskCreate(dmx_task, "DMX", 1024, NULL, 2, NULL);                                          //create task to listen for DMX frames
+    xTaskCreate(dmx_task, "DMX", 1024, NULL, 2, NULL);  // create task to listen for DMX frames
     httpd_init();
 
     vTaskDelete(NULL);
 }
-
-
 
 int main() {
     stdio_init_all();
