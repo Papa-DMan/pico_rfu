7196046d2d4ec596ef750f02eed2d8f204df72ea
Papa-DMan <66575559+Papa-DMan@users.noreply.github.com>
2023-10-10 17:36:37 -0400

    Fixed linking issues

--- bootloader/firmwareblobler.py
+++ bootloader/firmwareblobler.py
@@ -2,6 +2,21 @@ import sys
 import re
 import struct
 
+def hex_to_bin(hex_values, bin_file_path):
+    bin_data = b''
+    with open(bin_file_path, 'wb') as bin_file:
+        #for every 4 bytes in the hex string, decode the bytes and write them to the binary file
+        for i in range(0, len(hex_values), 4):
+            #convert from string to int, then to bytes
+            bytes = struct.pack('4B', *[int(hex_values[i+j], 16) for j in range(4)])
+            bin_data += bytes
+        bin_file.write(bin_data)
+            
+
+           
+            
+
+
 if len(sys.argv) < 3:
     print('Usage: python firmwareblober.py <input_file> <output_file>')
     sys.exit(1)
@@ -16,19 +31,7 @@ with open(input_file, 'r') as f:
 # Find the firmware data in the header file
 match = re.search(r'{([^}]+)}', header, re.DOTALL)
 if match:
-    firmware_data = match.group(1).replace('\n', '').replace(' ', '').split(',')
-    #print("Found firmware data in header file: ")
-    for byte in firmware_data:
-        #print(byte)
-
-    # Convert the firmware data array to binary
-    firmware_binary = b''
-    for byte in firmware_data:
-        firmware_binary += struct.pack('B', int(byte, 16))
-
-
-    # Write the binary data to a file
-    with open(output_file, 'wb') as f:
-        f.write(firmware_binary)
+    firmware_data = match.group(1).replace('\n', '').replace(' ', '').replace('\\', '').split(',')
+    hex_to_bin(firmware_data, output_file)
 else:
     print("Error: Could not find firmware data in header file")
--- bootloader/main.cpp
+++ bootloader/main.cpp
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/flash.h"
-#include "hardware/regs/rosc.h"
-#include "hardware/regs/addressmap.h"
+//#include "hardware/regs/rosc.h"
+//#include "hardware/regs/addressmap.h"
 #include "hardware/structs/rosc.h"
 #include "hardware/flash.h"
-#include "pico/cyw43_arch.h"
+//#include "pico/cyw43_arch.h"
 
 // Bootloader size is 128KB
 // Bootloader is 0x10000000 - 0x10002FFFF
-#define BOOTLOADER_SIZE 0x00020000
-#define FIRMWARE_START 0x10020000
+//#define BOOTLOADER_SIZE 0x00020000
+//#define FIRMWARE_START 0x10056BBE
 //flash is 2MB
 #define FIRMWARE_END 0x10200000
 
-#define WIFI_FIRMWARE_START 0x10200000
-#define WIFI_FIRMWARE_END 0x10240000
+#define WIFI_FIRMWARE_START 0x10020000
+#define WIFI_FIRMWARE_END 0x10056FD8
 
 bool validate_firmware(uint32_t firmware_start, uint32_t firmware_end) {
     return true;
@@ -22,19 +23,22 @@ bool validate_firmware(uint32_t firmware_start, uint32_t firmware_end) {
 
 
 int main() {
+    stdio_usb_init();
+    sleep_ms(10000);
     // Validate firmware
-    uint32_t firmware_start = *(uint32_t*)FIRMWARE_START;
-    uint32_t firmware_end = *(uint32_t*)FIRMWARE_END;
+    uint32_t firmware_start = *(uint32_t*)0x10056FD8;
+    uint32_t firmware_end = *(uint32_t*)0x10200000;
     if (firmware_start != 0xFFFFFFFF && firmware_end != 0xFFFFFFFF) {
+        printf("Firmware start: %x\n", firmware_start);
         // Firmware is valid, jump to it
         bool valid = validate_firmware(firmware_start, firmware_end);
         if (valid) {
+            printf("Firmware is valid\n");
             //disable interrupts
             asm volatile("cpsid i");
-
             //disable cache
             asm volatile("ldr r0, =0x0C000000");
-            asm volatile("mov r1, #0x1000");
+            asm volatile("ldr r1, =0x1000");
             asm volatile("str r1, [r0]");
 
             //disable mmu
@@ -44,7 +48,7 @@ int main() {
 
             //disable branch prediction
             asm volatile("ldr r0, =0x0C000000");
-            asm volatile("mov r1, #0x8000");
+            asm volatile("ldr r1, =0x8000");
             asm volatile("str r1, [r0, #4]");
 
             //flush data cache
@@ -72,8 +76,8 @@ int main() {
             asm volatile("ldr r1, =0xE000ED9C");
             asm volatile("str r0, [r1]");
 
-            // set VTOR to 
-            asm volatile("ldr r0, =FIRMWARE_START + 0x100");
+            // set VTOR [31:8] to 
+            asm volatile("ldr r0, =0x10056FD8 + 0x100");
             asm volatile("mov r1, #8");
             asm volatile("lsr r0, r0, r1");
             asm volatile("ldr r1, =0xE000ED08");
@@ -82,13 +86,13 @@ int main() {
             // set stack pointer
             asm volatile("ldr r0, [r0, #4]");
             asm volatile("mov sp, r0");
-
             // jump to firmware
             asm volatile("bx r0");
         }
         else {
             // Firmware is invalid, start OTA app
-            cyw43_arch_init();
+            printf("Firmware is invalid\n");
+            return 0;
         }
     }
 
